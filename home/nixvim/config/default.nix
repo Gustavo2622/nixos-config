c98ad620fa5d9f89310972977f052d08bf2ae91a
Gustavo Delerue <gxdelerue@proton.me>
2025-06-13 14:48:24 +0100

    Added conform.nvim

--- home/nixvim/config/default.nix
+++ home/nixvim/config/default.nix
@@ -62,6 +62,68 @@
 
   plugins.typescript-tools.enable = true;
 
+  # Setup formatting with conform.nvim
+  # FIXME:  check that this is working, debug on desktop
+  plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      formatters_by_ft = {
+	ocaml = [
+	  "ocamlformat"
+	];
+      };
+      formatters = {
+	ocamlformat = {
+	  prepend_args = [
+	    "--if-then-else"
+	    "vertical"
+	    "--break-cases"
+	    "fit-or-vertical"
+	    "--type-decl"
+	    "sparse"
+	  ];
+	};
+      };
+      log_level = "warn";
+      notify_on_error = false;
+      notify_no_formatters = false;
+      format_on_save = # Lua
+      ''
+	function(bufnr)
+	  if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+	    return
+	  end
+
+	  if slow_format_filetypes[vim.bo[bufnr].filetype] then
+	    return
+	  end
+
+	  local function on_format(err)
+	    if err and err:match("timeout$") then
+	      slow_format_filetypes[vim.bo[bufnr].filetype] = true
+	    end
+	  end
+
+	  return { timeout_ms = 200, lsp_fallback = true }, on_format
+	end
+      '';
+      format_after_save = # Lua
+      ''
+	function(bufnr)
+	  if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+	    return
+	  end 
+
+	  if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+	    return
+	  end
+
+	  return { lsp_fallback = true }
+	end
+      '';
+    };
+  };
+
   # Better LSP
   plugins.lspsaga = {
     enable = true;
